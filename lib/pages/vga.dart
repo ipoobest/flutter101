996e921059de5a82ae5efaffc4a3b88e639c80ef
poobest <poobest.pooh@gmail.com>
2019-09-17 16:34:07 +0700

    fetch data from server

--- lib/pages/vga.dart
+++ lib/pages/vga.dart
@@ -1,9 +1,32 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_cache_store/flutter_cache_store.dart';
 
 import 'package:hello_flutter/pages/vga_deail.dart';
 
-class VgaPage extends StatelessWidget {
+
+class VgaPage extends StatefulWidget {
+  @override
+  _VgaPageState createState() => _VgaPageState();
+}
+
+class _VgaPageState extends State<VgaPage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    const url = "https://www.advice.co.th/pc/get_comp/vga";
+    final store = await CacheStore.getInstance();
+    File file = await store.getFile(url);
+    print(file.readAsStringSync());
+    print('test');
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,15 +43,6 @@ class VgaPage extends StatelessWidget {
               )),
             child: Row(
               children: <Widget>[
-                Container(
-                  height: 150,
-                  width: 150,
-                  child: CachedNetworkImage(
-                    imageUrl: "http://via.placeholder.com/350x150",
-                    placeholder: (context, url) =>  CircularProgressIndicator(),
-                    errorWidget: (context, url, error) =>  Icon(Icons.error),
-              ),
-                ),
                 Text('$index'),
               ],
             ));
